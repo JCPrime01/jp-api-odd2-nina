3d04cd59dc211a024ef3c04f92121486de6d928e
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 15:21:07 -0300

    update shareCode to DGJ49AXLVOT

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=25MCLHLF2AC&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=DGJ49AXLVOT&afp=tapi2' });
     res.end();
   }
