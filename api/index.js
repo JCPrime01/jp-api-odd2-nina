cfafa6b094600ead07191ee31e31c682563cb42a
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 20:14:48 -0300

    chore: update shareCode to SH9HMIP7DM9

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=ACIICGMHS4K&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=SH9HMIP7DM9&afp=tapi2' });
     res.end();
   }
