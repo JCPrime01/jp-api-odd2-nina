7059168570ce11db0f04674541ff4fb962d8e19c
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 23:06:33 -0300

    chore: shareCode USYBYI6JLGJ -> H6SJFS2K47X (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=USYBYI6JLGJ&afp=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=H6SJFS2K47X&afp=ta' });
     res.end();
   }
